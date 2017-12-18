d1b2b63ff2b6ece08d03ee6eed0743cd12c655c4
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-12-18 14:07:20 -0500

    services working

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,41 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>()
 
   private recipes: Recipe[] = [
-    new Recipe('First Recipe', 'Description of first recipe', 'http://www.seriouseats.com/recipes/assets_c/2010/09/20100910-duck-27-thumb-625xauto-110803.jpg'),
-    new Recipe('Second Recipe', 'Description of Second recipe', 'http://www.seriouseats.com/recipes/assets_c/2010/09/20100910-duck-27-thumb-625xauto-110803.jpg')
+    new Recipe(
+      'First Recipe',
+      'Description of first recipe',
+      'http://www.seriouseats.com/recipes/assets_c/2010/09/20100910-duck-27-thumb-625xauto-110803.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe(
+      'Second Recipe',
+      'Description of Second recipe',
+      'https://upload.wikimedia.org/wikipedia/commons/8/8f/NYC-Diner-Bacon-Cheeseburger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
+    )
   ];
+
+  constructor(private slService: ShoppingListService) { }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ElementRef, EventEmitter, ViewChild, Output } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -10,9 +11,9 @@ import { Ingredient } from '../../shared/ingredient.model';
 export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput') nameInputRef: ElementRef
   @ViewChild('amountInput') amountInputRef: ElementRef
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  // @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
@@ -20,9 +21,10 @@ export class ShoppingEditComponent implements OnInit {
   onAddItem() {
     const ingName = this.nameInputRef.nativeElement.value
     const ingAmount = this.amountInputRef.nativeElement.value
-
     const newIngredient = new Ingredient(ingName, ingAmount)
-    this.ingredientAdded.emit(newIngredient)
+    // this.ingredientAdded.emit(newIngredient)
+
+    this.slService.addIngredient(newIngredient);
   }
 
 }
