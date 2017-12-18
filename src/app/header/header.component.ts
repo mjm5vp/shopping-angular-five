863371b316f782964d04d49eacc3b6ff97e38568
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-12-18 16:41:10 -0500

    routing complete

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,9 +6,9 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 
 export class HeaderComponent {
-  @Output() featureSelected = new EventEmitter<string>();
+  // @Output() featureSelected = new EventEmitter<string>();
 
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature)
-  }
+  // onSelect(feature: string) {
+  //   this.featureSelected.emit(feature)
+  // }
 }
